7ee1a142d9fab816410eba8e7167fe3b82f815fa
ChenguK <ChenguKargbo@gmail.com>
2020-08-03 10:13:43 -0400

    Deploy to Heroku

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const morgan = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+const port = process.env.PORT || 3000;
+
+require('dotenv').config();
+
+// create the Express app
+const app = express();
+
+// connect to the MongoDB with mongoose
+require('./config/database');
+require('./config/passport');
+
+// require our routes
+const indexRoutes = require('./routes/index');
+// const usersRoutes = require('./routes/users');
+
+// view engine setup
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+// TODO Add session middleware here
+app.use(session({
+    secret: "SEIRRocks!",
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', indexRoutes);
+// app.use('/', usersRoutes);
+
+app.listen(port, () => {
+    console.log(`Express is listening on port:${port}`);
+});
