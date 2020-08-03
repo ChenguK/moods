15f4541cdcdfdc33d311b23b9502608b45d50e5a
ChenguK <ChenguKargbo@gmail.com>
2020-08-03 19:53:50 -0400

    created new page

--- controllers/moods.js
+++ controllers/moods.js
@@ -2,6 +2,7 @@ const Mood = require("../models/mood");
 
 module.exports = {
     index,
+    create,
     newPost,
     editPost,
     delPost,
@@ -16,16 +17,29 @@ function index(req, res) {
     });
 }
 
-function newPost(req, res) {
+function create(req, res) {
     req.user.posts.push(req.body);
     req.user.save(function (err) {
         res.redirect("/moods");
     });
+}
+
+function newPost(req, res) {
+    res.render("moods/new", {
+        title: "New Mood",
+        user: req.user
+    });
 };
 
+
 function editPost(req, res) {
-
-
+    Mood.findById(req.user.body, function (err, mood) {
+        if (!mood.user.equals(req.user._id)) return req.redirect('/moods');
+        res.render("/moods/edit", {
+            mood,
+            user: req.user
+        });
+    });
 };
 
 function delPost(req, res) {
